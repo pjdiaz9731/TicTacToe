d71b86765a183a91a6d502dc1e23e19f924bd555
Pablo Díaz <pdiaz9731@gmail.com>
2024-01-05 19:42:05 -0600

    Creacion de proyecto e Interfaz

---
+++ src/main/java/com/mycompany/tictactoe1/TicTacToe1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.tictactoe1;
+
+/**
+ *
+ * @author pjdiaz
+ */
+public class TicTacToe1 {
+
+    
+    public static void main(String[] args) {
+     FormGame formGame = new FormGame();
+     formGame.setVisible(true);
+    }
+    
+}
