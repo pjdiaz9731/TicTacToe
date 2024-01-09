510a546343d6be5b082b5b3cb392a78afdea5fa8
Pablo Díaz <pdiaz9731@gmail.com>
2024-01-08 22:57:42 -0600

    Correcion de problema de imagene en el meni de inicio

--- src/main/java/com/mycompany/tictactoe1/FormGame.java
+++ src/main/java/com/mycompany/tictactoe1/FormGame.java
@@ -47,8 +47,6 @@ public class FormGame extends javax.swing.JFrame {
         Jugador2 = new javax.swing.JTextField();
         PanelBoton = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        imagen1 = new modelo.Imagen();
-        imagen2 = new modelo.Imagen();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
@@ -193,14 +191,6 @@ public class FormGame extends javax.swing.JFrame {
         PanelFondo.add(PanelBoton);
         PanelBoton.setBounds(140, 300, 160, 50);
 
-        imagen1.setText("imagen1");
-        PanelFondo.add(imagen1);
-        imagen1.setBounds(20, 140, 50, 50);
-
-        imagen2.setText("imagen2");
-        PanelFondo.add(imagen2);
-        imagen2.setBounds(30, 130, 70, 90);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -212,95 +202,96 @@ public class FormGame extends javax.swing.JFrame {
             .addComponent(PanelFondo, javax.swing.GroupLayout.PREFERRED_SIZE, 394, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
+        PanelFondo.getAccessibleContext().setAccessibleParent(PanelFondo);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void lblCierreMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseClicked
-        System.exit(0);
-    }//GEN-LAST:event_lblCierreMouseClicked
+    private void PanelFondoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_PanelFondoKeyTyped
+        char caracter = Character.toUpperCase(evt.getKeyChar());
+        evt.setKeyChar(caracter);
+        if(Jugador2.getText().length()>=8)
+        evt.consume();
+    }//GEN-LAST:event_PanelFondoKeyTyped
 
-    private void lblCierreMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseEntered
-        lblCierre.setForeground(Color.RED); //Cambio de color cuando el mouse esta sobre la X
-    }//GEN-LAST:event_lblCierreMouseEntered
+    private void PanelFondoMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PanelFondoMousePressed
+        PanelFondo.requestFocus();
+    }//GEN-LAST:event_PanelFondoMousePressed
 
-    private void lblCierreMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseExited
-        lblCierre.setForeground(new Color(255, 153, 153)); //Cambio de color cuando el mouse  no esta sobre la X
-    }//GEN-LAST:event_lblCierreMouseExited
+    private void jLabel1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseExited
+        PanelBoton.setBackground (new Color(42,22,79));//Cambio de color cuando el mouse esta fuera el Boton de Inicio
+    }//GEN-LAST:event_jLabel1MouseExited
+
+    private void jLabel1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseEntered
+        PanelBoton.setBackground (new Color(126,46,196));//Cambio de color cuando el mouse esta sobre el Boton de Inicio
+    }//GEN-LAST:event_jLabel1MouseEntered
 
     private void Jugador2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Jugador2ActionPerformed
-       
+
     }//GEN-LAST:event_Jugador2ActionPerformed
 
-    private void Jugador1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Jugador1ActionPerformed
-        
-    }//GEN-LAST:event_Jugador1ActionPerformed
-
-    private void Jugador1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador1MouseEntered
-        Border border = BorderFactory.createLineBorder(new Color(52, 136, 235), 2);
-        Jugador1.setBorder(border);//Cambio de color cuando el mouse esta sobre el jugador 1 
-    }//GEN-LAST:event_Jugador1MouseEntered
-
-    private void Jugador1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador1MouseExited
-        Jugador1.setBorder(null);
-    }//GEN-LAST:event_Jugador1MouseExited
+    private void Jugador2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador2MouseExited
+        Jugador2.setBorder(null);
+    }//GEN-LAST:event_Jugador2MouseExited
 
     private void Jugador2MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador2MouseEntered
         Border border = BorderFactory.createLineBorder(new Color(165, 40, 174), 2);
         Jugador2.setBorder(border);//Cambio de color cuando el mouse esta sobre el jugador 2
     }//GEN-LAST:event_Jugador2MouseEntered
 
-    private void Jugador2MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador2MouseExited
-        Jugador2.setBorder(null);
-    }//GEN-LAST:event_Jugador2MouseExited
-
-    private void Jugador1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador1FocusGained
-        if (Jugador1.getText().equals("JUGADOR1"))//Eliminar texto para que el usuario pueda copletar nombre al dar clic en Jugador1
-            Jugador1.setText("");//Volver a Poner "Jugador1" su el usuario no completa la casilla 
-    }//GEN-LAST:event_Jugador1FocusGained
+    private void Jugador2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador2FocusLost
+        if (Jugador2.getText().equals(""))
+        Jugador2.setText("JUGADOR2");//Volver a Poner "Jugador2" su el usuario no completa la casilla
+    }//GEN-LAST:event_Jugador2FocusLost
 
     private void Jugador2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador2FocusGained
         if (Jugador2.getText().equals("JUGADOR2"))//Eliminar texto para que el usuario pueda copletar nombre al dar clic en Jugador2
-            Jugador2.setText("");
+        Jugador2.setText("");
     }//GEN-LAST:event_Jugador2FocusGained
+
+    private void Jugador1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_Jugador1KeyTyped
+        char caracter = Character.toUpperCase(evt.getKeyChar());
+        evt.setKeyChar(caracter);
+        if(Jugador1.getText().length()>=8)
+        evt.consume();
+
+    }//GEN-LAST:event_Jugador1KeyTyped
+
+    private void Jugador1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Jugador1ActionPerformed
+
+    }//GEN-LAST:event_Jugador1ActionPerformed
+
+    private void Jugador1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador1MouseExited
+        Jugador1.setBorder(null);
+    }//GEN-LAST:event_Jugador1MouseExited
+
+    private void Jugador1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_Jugador1MouseEntered
+        Border border = BorderFactory.createLineBorder(new Color(52, 136, 235), 2);
+        Jugador1.setBorder(border);//Cambio de color cuando el mouse esta sobre el jugador 1
+    }//GEN-LAST:event_Jugador1MouseEntered
 
     private void Jugador1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador1FocusLost
         if (Jugador1.getText().equals("")) {
             Jugador1.setText("JUGADOR1");//Volver a Poner "Jugador1" su el usuario no completa la casilla
         }
-
     }//GEN-LAST:event_Jugador1FocusLost
 
-    private void Jugador2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador2FocusLost
-        if (Jugador2.getText().equals(""))
-            Jugador2.setText("JUGADOR2");//Volver a Poner "Jugador2" su el usuario no completa la casilla
-    }//GEN-LAST:event_Jugador2FocusLost
+    private void Jugador1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_Jugador1FocusGained
+        if (Jugador1.getText().equals("JUGADOR1"))//Eliminar texto para que el usuario pueda copletar nombre al dar clic en Jugador1
+        Jugador1.setText("");//Volver a Poner "Jugador1" su el usuario no completa la casilla
+    }//GEN-LAST:event_Jugador1FocusGained
 
-    private void PanelFondoMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PanelFondoMousePressed
-        PanelFondo.requestFocus();
-    }//GEN-LAST:event_PanelFondoMousePressed
+    private void lblCierreMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseExited
+        lblCierre.setForeground(new Color(255, 153, 153)); //Cambio de color cuando el mouse  no esta sobre la X
+    }//GEN-LAST:event_lblCierreMouseExited
 
-    private void Jugador1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_Jugador1KeyTyped
-       char caracter = Character.toUpperCase(evt.getKeyChar());
-       evt.setKeyChar(caracter);
-       if(Jugador1.getText().length()>=8)
-           evt.consume();
-           
-    }//GEN-LAST:event_Jugador1KeyTyped
+    private void lblCierreMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseEntered
+        lblCierre.setForeground(Color.RED); //Cambio de color cuando el mouse esta sobre la X
+    }//GEN-LAST:event_lblCierreMouseEntered
 
-    private void PanelFondoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_PanelFondoKeyTyped
-       char caracter = Character.toUpperCase(evt.getKeyChar());
-       evt.setKeyChar(caracter);
-       if(Jugador2.getText().length()>=8)
-           evt.consume();
-    }//GEN-LAST:event_PanelFondoKeyTyped
-
-    private void jLabel1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseEntered
-       PanelBoton.setBackground (new Color(126,46,196));//Cambio de color cuando el mouse esta sobre el Boton de Inicio 
-    }//GEN-LAST:event_jLabel1MouseEntered
-
-    private void jLabel1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseExited
-       PanelBoton.setBackground (new Color(42,22,79));//Cambio de color cuando el mouse esta fuera el Boton de Inicio 
-    }//GEN-LAST:event_jLabel1MouseExited
+    private void lblCierreMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblCierreMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_lblCierreMouseClicked
 
     /**
      * @param args the command line arguments
@@ -344,8 +335,6 @@ public class FormGame extends javax.swing.JFrame {
     private javax.swing.JPanel PanelBoton;
     private javax.swing.JPanel PanelFondo;
     private javax.swing.JLabel Titulo;
-    private modelo.Imagen imagen1;
-    private modelo.Imagen imagen2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lblCierre;
     // End of variables declaration//GEN-END:variables
