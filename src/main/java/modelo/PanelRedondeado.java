2ea5876bcae9298449b86ea50bb7148a29594e52
Pablo Díaz <pdiaz9731@gmail.com>
2024-01-11 23:57:27 -0600

    Arcos Panel Redondeado

--- src/main/java/modelo/PanelRedondeado.java
+++ src/main/java/modelo/PanelRedondeado.java
@@ -1,12 +1,18 @@
 
 package modelo;
 
+import java.awt.Color;
+import javax.swing.JPanel;
+        
 
-public class PanelRedondeado {
   
-    private class PanelRedondeado extends Jpanel{
-    
+ 
+    public class PanelRedondeado extends JPanel{
+   private double arcoAncho = 50;
+   private double arcoAltura = 50;
+   private Color colorBonrde = new Color (165,40,174);
+   
         
         
     }
-}
+
